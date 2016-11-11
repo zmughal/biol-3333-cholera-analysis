9349d130ecb24109bfdba6c72e91c487dbf08707
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-11-11 13:58:01 -0600

    Update with graphs

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,13 @@
 # Example preprocessing script.
+tracking.a$Date <- as.Date(tracking.a$Date)
+A <- c(NA, diff(tracking.a$Cases))
+tracking.a$CleanDeltaCases  <- ifelse( is.na(A), tracking.a$DeltaCases, A)
+tracking.a$CleanDeltaDeaths <- c(NA, diff(tracking.a$Deaths))
+
+tracking.b$Date <- as.Date(tracking.b$Date)
+tracking.b$CleanDeltaCases  <- tracking.b$DeltaCases
+tracking.b$CleanDeltaDeaths <- tracking.b$DeltaDeaths
+
+tracking.c$Date <- as.Date(tracking.c$Date)
+tracking.c$CleanDeltaCases  <- tracking.c$DeltaCases
+tracking.c$CleanDeltaDeaths <- tracking.c$DeltaDeaths
--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,42 @@
 library('ProjectTemplate')
 load.project()
 
+library(ggplot2)
+
 for (dataset in project.info$data)
 {
   message(paste('Showing top 5 rows of', dataset))
+  plot <- ggplot(get(dataset), aes(Date,DeltaCases)) + geom_line()
+  #ggsave(file.path('graphs', 'plot1.pdf'))
+  #print(plot)
   print(head(get(dataset)))
 }
+
+keep_cols <- c('Date','CleanDeltaCases','CleanDeltaDeaths')
+tracking.stack.all <- rbind(
+		tracking.a[,keep_cols],
+		tracking.b[,keep_cols],
+		tracking.c[,keep_cols]
+	)
+tracking.agg <- aggregate( x = tracking.stack.all, by = list(tracking.stack.all$Date), FUN = 'mean' )
+
+
+plot.all.delta.cases <- ggplot(tracking.agg, aes(Date,CleanDeltaCases)) + geom_line()
+#print(plot.all.delta.cases)
+ggsave( file.path('graphs', 'plot.all.delta.cases.png'))
+
+plot.all.delta.deaths <- ggplot(tracking.agg, aes(Date,CleanDeltaDeaths)) + geom_line()
+#print(plot.all.delta.deaths)
+ggsave( file.path('graphs', 'plot.all.delta.deaths.png'))
+
+plot.all.cum.delta.cases <- ggplot(
+	subset( tracking.agg, !is.na(CleanDeltaCases) ),
+	aes(Date,cumsum(CleanDeltaCases))) + geom_line()
+#print(plot.all.cum.delta.cases)
+ggsave( file.path('graphs', 'plot.all.cum.delta.cases.png'))
+
+plot.all.cum.delta.deaths <- ggplot(
+	subset( tracking.agg, !is.na(CleanDeltaDeaths) ),
+	aes(Date,cumsum(CleanDeltaDeaths))) + geom_line()
+#print(plot.all.cum.delta.deaths)
+ggsave( file.path('graphs', 'plot.all.cum.delta.deaths.png'))
