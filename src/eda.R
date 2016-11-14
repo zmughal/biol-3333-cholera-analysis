2e8d3804ae5f04ec318b143d56c316e4c27925e6
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-11-14 11:10:05 -0600

    Add a vertical line for Hurricane Matthew's Haiti landfall

--- src/eda.R
+++ src/eda.R
@@ -20,23 +20,32 @@ tracking.stack.all <- rbind(
 	)
 tracking.agg <- aggregate( x = tracking.stack.all, by = list(tracking.stack.all$Date), FUN = 'mean' )
 
+# Date for the Haiti landfall of Hurricane Matthew
+hurricane_matthew_date <- as.Date('2016-10-07')
+hurricane_matthew_date_vline <- geom_vline(xintercept=as.numeric(hurricane_matthew_date), linetype=4)
 
-plot.all.delta.cases <- ggplot(tracking.agg, aes(Date,CleanDeltaCases)) + geom_line()
+plot.all.delta.cases <- ggplot(tracking.agg, aes(Date,CleanDeltaCases)) +
+	geom_line() +
+	hurricane_matthew_date_vline
 #print(plot.all.delta.cases)
 ggsave( file.path('graphs', 'plot.all.delta.cases.png'))
 
-plot.all.delta.deaths <- ggplot(tracking.agg, aes(Date,CleanDeltaDeaths)) + geom_line()
-#print(plot.all.delta.deaths)
-ggsave( file.path('graphs', 'plot.all.delta.deaths.png'))
+#plot.all.delta.deaths <- ggplot(tracking.agg, aes(Date,CleanDeltaDeaths)) +
+	#geom_line() +
+	#hurricane_matthew_date_vline
+##print(plot.all.delta.deaths)
+#ggsave( file.path('graphs', 'plot.all.delta.deaths.png'))
 
 plot.all.cum.delta.cases <- ggplot(
 	subset( tracking.agg, !is.na(CleanDeltaCases) ),
-	aes(Date,cumsum(CleanDeltaCases))) + geom_line()
+	aes(Date,cumsum(CleanDeltaCases))) + geom_line() +
+	hurricane_matthew_date_vline
 #print(plot.all.cum.delta.cases)
 ggsave( file.path('graphs', 'plot.all.cum.delta.cases.png'))
 
-plot.all.cum.delta.deaths <- ggplot(
-	subset( tracking.agg, !is.na(CleanDeltaDeaths) ),
-	aes(Date,cumsum(CleanDeltaDeaths))) + geom_line()
-#print(plot.all.cum.delta.deaths)
-ggsave( file.path('graphs', 'plot.all.cum.delta.deaths.png'))
+#plot.all.cum.delta.deaths <- ggplot(
+	#subset( tracking.agg, !is.na(CleanDeltaDeaths) ),
+	#aes(Date,cumsum(CleanDeltaDeaths))) + geom_line() +
+	#hurricane_matthew_date_vline
+##print(plot.all.cum.delta.deaths)
+#ggsave( file.path('graphs', 'plot.all.cum.delta.deaths.png'))
